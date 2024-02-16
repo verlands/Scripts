56cb0c002706e41279c1ddba123d3c8df5a08880
verlands <157267830+verlands@users.noreply.github.com>
2024-02-16 17:01:55 +0800

    Update qb_install.sh

--- qb_install.sh
+++ qb_install.sh
@@ -221,84 +221,160 @@ fi
 
 
 
-# ################################################
-# ###
-# ###     安装 autobrr 并设置守护进程
-# ###
-# ################################################
+################################################
+###
+###     安装 autobrr 并设置守护进程
+###
+################################################
 
-# # 下载autobrr
-# wget $(curl -s https://api.github.com/repos/autobrr/autobrr/releases/latest | grep download | grep linux_x86_64 | cut -d\" -f4)
+# 下载autobrr
+wget $(curl -s https://api.github.com/repos/autobrr/autobrr/releases/latest | grep download | grep linux_x86_64 | cut -d\" -f4)
 
-# # 解压 autobrr 和 autobrrctl 到 /usr/local/bin，如果不是root用户，可以解压到 ~/.bin 目录内
-# sudo tar -C /usr/local/bin -xzf autobrr*.tar.gz
+# 解压 autobrr 和 autobrrctl 到 /usr/local/bin，如果不是root用户，可以解压到 ~/.bin 目录内
+sudo tar -C /usr/local/bin -xzf autobrr*.tar.gz
 
-# # 创建配置目录
-# mkdir -p ~/.config/autobrr
+# 创建配置目录
+mkdir -p ~/.config/autobrr
 
-# # 配置文件
-# CONFIG_FILE="/root/.config/autobrr/config.toml"
-# curl -o $CONFIG_FILE -L https://raw.githubusercontent.com/autobrr/autobrr/develop/config.toml
-# # 修改配置文件，使得可以远程连接
-# sudo sed -i 's/host = "127.0.0.1"/host = "0.0.0.0"/' $CONFIG_FILE
-# # 修改网页端口默认端口
-# sudo sed -i 's/port = 7474/port = 12381/' $CONFIG_FILE
+# 配置文件
+CONFIG_FILE="/root/.config/autobrr/config.toml"
+curl -o $CONFIG_FILE -L https://raw.githubusercontent.com/autobrr/autobrr/develop/config.toml
+# 修改配置文件，使得可以远程连接
+sudo sed -i 's/host = "127.0.0.1"/host = "0.0.0.0"/' $CONFIG_FILE
+# 修改网页端口默认端口
+sudo sed -i 's/port = 7474/port = 12381/' $CONFIG_FILE
 
-# # 创建 systemd 服务文件
-# AUTOBRR_SERVICE=/etc/systemd/system/autobrr.service
-# sudo tee $AUTOBRR_SERVICE <<EOF
-# [Unit]
-# Description=autobrr daemon
-# After=network-online.target
+# 创建 systemd 服务文件
+AUTOBRR_SERVICE=/etc/systemd/system/autobrr.service
+sudo tee $AUTOBRR_SERVICE <<EOF
+[Unit]
+Description=autobrr daemon
+After=network-online.target
 
-# [Service]
-# User=root
-# Group=root
-# Type=simple
-# ExecStart=/usr/local/bin/autobrr --config=/root/.config/autobrr/
-# Restart=on-failure
-# RestartSec=5
+[Service]
+User=root
+Group=root
+Type=simple
+ExecStart=/usr/local/bin/autobrr --config=/root/.config/autobrr/
+Restart=on-failure
+RestartSec=5
 
-# [Install]
-# WantedBy=multi-user.target
-# EOF
+[Install]
+WantedBy=multi-user.target
+EOF
 
-# # 提示服务单元文件已创建
-# echo "Autobrr 守护进程已创建：$AUTOBRR_SERVICE"
+# 提示服务单元文件已创建
+echo "Autobrr 守护进程已创建：$AUTOBRR_SERVICE"
 
-# # 更新配置
-# systemctl daemon-reload
 
-# # 添加 qb 和 autobrr 命令别名
-# echo "alias qb=\"service qb\"" >> ~/.zshrc
-# echo "alias auto=\"service autobrr\"" >> ~/.zshrc
-# # 使得别名生效
-# source ~/.zshrc
 
-# # 启用qbittorrent-nox服务
-# systemctl enable qb.service
-# systemctl enable autobrr
 
-# # 启动qbittorrent-nox服务
-# systemctl start qb.service
-# systemctl start autobrr
 
-# # 提示启用和启动服务的命令
-# echo "qbittorrent-nox 和 autobrr 服务已启用和启动。"
-# echo "qb enable: 设置开机启动"
-# echo "qb disable: 关闭开机启动"
-# echo "qb start: 开启服务"
-# echo "qb stop: 关闭服务"
-# echo "qb restart: 重启服务"
-# echo "qb status: 查看服务状态"
-# echo "autobrr同理，只需要将上面命令里面的 qb 改为 auto"
+################################################
+###
+###     安装 autoremove-torrents 并设置定期运行
+###
+################################################
 
-# # 获取本机IP地址
-# ip_address=$(hostname -I | awk '{print $1}')
+# 检查 pip 是否安装
+if ! command -v pip > /dev/null; then
+    echo "pip 未安装，尝试安装 pip..."
+    sudo apt-get update -y && sudo apt-get install -y python3-pip
+    if [ $? -ne 0 ]; then
+        echo "pip 安装失败，退出。"
+        exit 1
+    fi
+else
+    echo "pip 已经安装。"
+fi
 
-# # 提示打开qbittorrent的命令
-# echo "要访问qbittorrent，请在浏览器中打开：http://$ip_address:12380"
-# echo "要访问 autobrr，请在浏览器中打开：http://$ip_address:12381"
+# 安装 autoremove-torrents
+echo "正在安装 autoremove-torrents..."
+pip -q install autoremove-torrents
+if [ $? -ne 0 ]; then
+    echo "autoremove-torrents 安装失败，退出。"
+    exit 1
+fi
+
+# 创建配置文件夹
+art_config_folder="/root/.config/autoremove-torrents"
+if [ ! -d "$art_config_folder" ]; then
+    echo "正在创建配置目录..."
+    mkdir -p "$art_config_folder"
+fi
+# 创建 autoremove-torrents 的 logs 目录
+art_log_folder="/root/.config/autoremove-torrents/logs"
+mkdir -p $art_log_folder
+
+# 创建或覆盖 config.yml 文件
+art_config_file="${art_config_folder}/config.yml"
+echo "正在写入配置文件到 ${art_config_file}..."
+cat > "$art_config_file" <<EOL
+qb_task:
+  client: qbittorrent
+  host: http://127.0.0.1:12380/
+  username: $username
+  password: $password
+  strategies:
+    Disk:
+      free_space:
+        min: 2
+        path: /root/
+        action: remove-old-seeds
+  delete_data: true
+EOL
+
+# 检查 autoremove-torrents 的路径，并设置计划任务
+echo "正在设置计划任务..."
+art_path=$(which autoremove-torrents)
+
+( crontab -l 2>/dev/null | grep -v "$art_path"; echo "*/1 * * * * $art_path --conf=${art_config_file} --log=${art_log_folder}" ) | crontab -
+
+# 检查计划任务是否设置成功
+if [ $? -ne 0 ]; then
+    echo "添加或更新计划任务失败，退出。"
+    exit 1
+fi
+
+echo "autoremove-torrents 设置成功，将每 1 分钟运行一次。"
+
+
+
+
+
+# 更新配置
+systemctl daemon-reload
+
+# 添加 qb 和 autobrr 命令别名
+echo "alias qb=\"service qb\"" >> ~/.zshrc
+echo "alias auto=\"service autobrr\"" >> ~/.zshrc
+# 使得别名生效
+source ~/.zshrc
+
+# 启用qbittorrent-nox服务
+systemctl enable qb.service
+systemctl enable autobrr
+
+# 启动qbittorrent-nox服务
+systemctl start qb.service
+systemctl start autobrr
+
+# 提示启用和启动服务的命令
+echo "qbittorrent-nox 和 autobrr 服务已启用和启动。"
+echo "qb enable: 设置开机启动"
+echo "qb disable: 关闭开机启动"
+echo "qb start: 开启服务"
+echo "qb stop: 关闭服务"
+echo "qb restart: 重启服务"
+echo "qb status: 查看服务状态"
+echo "autobrr同理，只需要将上面命令里面的 qb 改为 auto"
+
+# 获取本机IP地址
+ip_address=$(hostname -I | awk '{print $1}')
+
+# 提示打开qbittorrent的命令
+echo "要访问qbittorrent，请在浏览器中打开：http://$ip_address:12380"
+echo "要访问 autobrr，请在浏览器中打开：http://$ip_address:12381"
 
 
 
